9da6838d2666ebb1b63f45972f31570de1a471bf
ASchalley <aschalley@unomaha.edu>
2024-12-13 18:35:20 -0600

    Finished

--- television.py
+++ television.py
@@ -20,6 +20,21 @@ class Television(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.onoffButton.clicked.connect(lambda : self.power())
+        self.muteButton.clicked.connect(lambda: self.mute())
+        self.volumeUp.clicked.connect(lambda: self.volume_up())
+        self.volumeDown.clicked.connect(lambda: self.volume_down())
+        self.channelUp.clicked.connect(lambda: self.channel_up())
+        self.channelDown.clicked.connect(lambda: self.channel_down())
+        self.channelButton1.clicked.connect(lambda: self.set_channel(1))
+        self.channelButton2.clicked.connect(lambda: self.set_channel(2))
+        self.channelButton3.clicked.connect(lambda: self.set_channel(3))
+        self.channelButton4.clicked.connect(lambda: self.set_channel(4))
+        self.channelButton5.clicked.connect(lambda: self.set_channel(5))
+        self.channelButton6.clicked.connect(lambda: self.set_channel(6))
+        self.channelButton7.clicked.connect(lambda: self.set_channel(7))
+        self.channelButton8.clicked.connect(lambda: self.set_channel(8))
+        self.channelButton9.clicked.connect(lambda: self.set_channel(9))
+        self.channelButton0.clicked.connect(lambda: self.set_channel(0))
 
     def power(self) -> None:
         """
@@ -39,8 +54,10 @@ class Television(QMainWindow, Ui_MainWindow):
         if self.__status:
             if self.__muted:
                 self.__muted = False
+                self.muteDisplay.setText("Mute: OFF")
             else:
                 self.__muted = True
+                self.muteDisplay.setText("Mute: ON")
 
     def channel_up(self) -> None:
         """
@@ -49,8 +66,10 @@ class Television(QMainWindow, Ui_MainWindow):
         if self.__status:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
+                self.channelDisplay.setText(f"Channel: {self.__channel}")
             else:
                 self.__channel = Television.MIN_CHANNEL
+                self.channelDisplay.setText(f"Channel: {self.__channel}")
 
     def channel_down(self) -> None:
         """
@@ -59,8 +78,19 @@ class Television(QMainWindow, Ui_MainWindow):
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
+                self.channelDisplay.setText(f"Channel: {self.__channel}")
             else:
                 self.__channel = Television.MAX_CHANNEL
+                self.channelDisplay.setText(f"Channel: {self.__channel}")
+
+    def set_channel(self, desiredchannel) -> None:
+        """
+        Method to return to the previous channel entered.
+        """
+        if self.__status:
+            self.__channel = desiredchannel
+            self.channelDisplay.setText(f"Channel: {self.__channel}")
+
 
     def volume_up(self) -> None:
         """
@@ -68,8 +98,10 @@ class Television(QMainWindow, Ui_MainWindow):
         """
         if self.__status:
             self.__muted = False
+            self.muteDisplay.setText("Mute: OFF")
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
+                self.volumeDisplay.setText(f"Volume: {self.__volume}")
 
     def volume_down(self) -> None:
         """
@@ -77,15 +109,7 @@ class Television(QMainWindow, Ui_MainWindow):
         """
         if self.__status:
             self.__muted = False
+            self.muteDisplay.setText("Mute: OFF")
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
-
-    def __str__(self) -> str:
-        """
-        Method to display tv status.
-        :return: tv status.
-        """
-        if self.__muted:
-            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = 0"
-        else:
-            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
+                self.volumeDisplay.setText(f"Volume: {self.__volume}")
